07fcb6314348a0bd8ae82ee9803a860d5ec7d310
yangtingxiao <yangtingxiao@gmail.com>
2020-09-24 09:41:22 +0800

    remote test

--- scripts/remote/runRemoteScript.js
+++ scripts/remote/runRemoteScript.js
@@ -5,7 +5,7 @@ const $ = new Env('京东白条远程执行测试')
   let script_text = null
   await $.getScript(url).then((script) => (script_text = script))
   {
-    console.log(script_text);
+    //console.log(script_text);
     await new Promise((resolve) => {
       try {
         eval(script_text)
