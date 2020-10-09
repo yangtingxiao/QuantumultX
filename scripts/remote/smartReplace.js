b0c8f0c21c0fff56ee166b0e82b616f5763821c1
yangtingxiao <yangtingxiao@gmail.com>
2020-10-09 10:23:20 +0800

    修复Cookies分隔符问题

--- scripts/remote/smartReplace.js
+++ scripts/remote/smartReplace.js
@@ -1,4 +1,4 @@
-
+let SPLITSTR = '&';
 const Secrets = {
   COOKIE: process.env.COOKIE, //cokie,多个用&隔开即可
   REMOTE_URL: process.env.REMOTE_URL, //签到地址,方便随时变动
@@ -6,10 +6,15 @@ const Secrets = {
   MULT_CUSTOM_REPLACE : process.env.MULT_CUSTOM_REPLACE || '[]' //多账号自定义替换  //需要和cookie相对应 //比如多账号替换助力码
 };
 
-let SPLITSTR = '&';
-
 async function replaceText(content, index) {
-  //console.log(Secrets.CUSTOM_REPLACE)
+  const SPLITARR = ['&','@','\n'];
+  for (let i = 0;i < SPLITARR.length;i ++) {
+    if (Secrets.COOKIE.indexOf(SPLITARR[i]) > -1) {
+      SPLITSTR = SPLITARR[i];
+      break;
+    }
+  }
+
   const replacements = eval(Secrets.CUSTOM_REPLACE) ;
   //console.log(replacements)
   const replacementslist = eval(Secrets.MULT_CUSTOM_REPLACE);
@@ -20,13 +25,6 @@ async function replaceText(content, index) {
     }
   }
 
-  const SPLITARR = ['&','@','\n'];
-  for (let i = 0;i < SPLITARR.length;i ++) {
-    if (Secrets.COOKIE.indexOf(SPLITARR[i]) > -1) {
-      SPLITSTR = SPLITARR[i];
-      break;
-    }
-  }
   if (content) {
     if (!content.match(/sendNotify\.js/)) {
       replacements.push({
@@ -38,14 +36,12 @@ async function replaceText(content, index) {
       replacements.push({key: "require('./jdCookie.js')", value: `['${Secrets.COOKIE.split(SPLITSTR)[index]}']` });
     }
   }
-  //console.log(replacements)
   return batchReplace(content, replacements);
 }
 function batchReplace(content, replacements) {
   for (let i = 0; i < replacements.length; i++) {
     content = content.replace(replacements[i].key, replacements[i].value);
   }
-  console.log(content);
   return content;
 }
 
